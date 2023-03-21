03d26b0ea0a1f8d1ee907326b52bb3467d091e49
LeoUpperThrower4 <ldsds94@gmail.com>
2023-03-21 09:20:14 -0300

    fix: new body structure

--- services/GPTestClient.js
+++ services/GPTestClient.js
@@ -3,12 +3,18 @@ const axios = require('axios');
 
 function GetUnitTest(func, rapidAPIKey) {
   return axios
-    .post(`${gatewayURL}/api/v0/unit-test-generation`, func, {
-      headers: {
-        'X-RapidAPI-Key': rapidAPIKey,
-        'X-RapidAPI-Host': gatewayProxyHost,
+    .post(
+      `${gatewayURL}/api/v0/unit-test-generation`,
+      {
+        code: func,
       },
-    })
+      {
+        headers: {
+          'X-RapidAPI-Key': rapidAPIKey,
+          'X-RapidAPI-Host': gatewayProxyHost,
+        },
+      }
+    )
     .catch((error) => {
       console.log('Error: ' + error);
       throw new Error(error);
